e41c27bb1c7c2b0fa583c5bb1b4dc3909e414a49
Marcelinho <130232436+marc3linho@users.noreply.github.com>
2023-05-10 16:37:27 +0200

    some wifi testing

--- src/orangeClockFunctions/displayBlockMoscowFees.py
+++ src/orangeClockFunctions/displayBlockMoscowFees.py
@@ -28,12 +28,15 @@ labelBlockCol = 75
 labelFeeRow = 110
 labelFeeCol = 40
 
+
 def connectWIFI():
+
+    global wifi
     wifi = network.WLAN(network.STA_IF)
     wifi.active(True)
     wifi.connect(secrets.SSID, secrets.PASSWORD)
     print(wifi.isconnected())
-     
+    
 def getMoscowTime():
     data = urequests.get("https://price.bisq.wiz.biz/getAllMarketPrices")
     jsonData = data.json()
@@ -62,19 +65,28 @@ def getMempoolFeesString():
 def displayInit():
     refresh(ssd, True)
     ssd.wait_until_ready()
-    time.sleep(180)
+    time.sleep(60)
     ssd._full = False
     ssd.wait_until_ready()
     refresh(ssd, True)
     ssd.wait_until_ready()
     ssd.sleep() #deep sleep
     time.sleep(25)
-         
+        
 def main():
-    connectWIFI()
-    displayInit()
+    global wifi
+    issue = False
     i = 1
+    
+    connectWIFI()
+    
+    displayInit()
+    
     while True:
+        if issue:
+            issue = False 
+            refresh(ssd, False)
+            time.sleep(3)
         if i > 72:
             i = 1
             ssd._full = True
@@ -86,14 +98,33 @@ def main():
             ssd.wait_until_ready()
             refresh(ssd, True)
             time.sleep(25)
-
-        Label(wri_small, labelBlockRow, labelBlockCol, "Block: " + getLastBlock())      
-        Label(wri_large, labelMoscowTimeRow, labelMoscowTimeCol, getMoscowTime())
-        Label(wri_tiny, labelFeeRow, labelFeeCol, getMempoolFeesString())
+            
+        try:
+            Label(wri_small, labelBlockRow, labelBlockCol, "Block: " + getLastBlock())      
+        except OSError:
+            Label(wri_small, labelBlockRow, labelBlockCol, "connection issue")
+            issue = True
+        try:    
+            Label(wri_large, labelMoscowTimeRow, labelMoscowTimeCol, getMoscowTime())
+        except OSError:
+            Label(wri_small, labelMoscowTimeRow, labelMoscowTimeCol, "connection issue")
+            issue = True
+        try:    
+            Label(wri_tiny, labelFeeRow, labelFeeCol, getMempoolFeesString())
+        except OSError:
+            Label(wri_tiny, labelFeeRow, labelFeeCol, "connection issue")
+            issue = True
         refresh(ssd, False)
         ssd.wait_until_ready()
         ssd.sleep() 
-        time.sleep(600)
+        if not issue:
+            time.sleep(600)
+        else:
+            wifi.disconnect()
+            wifi.connect(secrets.SSID, secrets.PASSWORD)
+            time.sleep(1)
+            print("wifi:" + str(wifi.isconnected()))
+            
         i = i + 1
         
 main()
