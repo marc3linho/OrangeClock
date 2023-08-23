c7ec45e7c717a2744d06ff756660847732287812
Marcelinho <130232436+marc3linho@users.noreply.github.com>
2023-08-23 11:19:01 +0200

    change fonts

--- src/orangeClockFunctions/layoutExample.py
+++ src/orangeClockFunctions/layoutExample.py
@@ -1,44 +1,64 @@
-from color_setup import ssd
+rom color_setup import ssd
 from gui.core.writer import Writer
 from gui.core.nanogui import refresh
 from gui.widgets.label import Label
 
-import gui.fonts.satoshiSymbol70 as satoshi
-import gui.fonts.freesans70 as large
-import gui.fonts.freesans20 as small
-import gui.fonts.freesans15 as tiny
+import time
+import gui.fonts.orangeClockIcons24 as iconsSmall
+import gui.fonts.orangeClockIcons44 as iconsLarge
+import gui.fonts.libreFranklinBold56 as large
+import gui.fonts.libreFranklinBold24 as small
 
-wri_satoshi = Writer(ssd, satoshi, verbose=False)
+
+wri_iconsLarge = Writer(ssd, iconsLarge, verbose=False)
+wri_iconsSmall = Writer(ssd, iconsSmall, verbose=False)
 wri_large = Writer(ssd, large, verbose=False)
 wri_small = Writer(ssd, small, verbose=False)
-wri_tiny = Writer(ssd, tiny, verbose=False)
+
 
 labelRow1 = 5
-labelCol1 = 75
-labelRow2 = 30
-labelCol2 = 45
-labelRow3 = 110
-labelCol3 = 40
+labelRow2 = 40
+labelRow3 = 100
+
 # Display 296*128
 displayLength = 296
 displayHeight = 128
-blockHeight = "Block: 803724"
-moscowTime = "3773"
-satoshiSymbol = "1"
-mempoolFees = "Fees[sat/vB] L:9 M:11 H:14"
+blockHeight = "804480" #A
+moscowTime = "383" #D
+mempoolFees = "L:8 M:9 H:10" #C
+dollarSats = "26,000" #H        
 
 
 def main():
-    print("Length blockHeight: " + str(Writer.stringlen(wri_large, blockHeight)))
-    print("Length moscowTime: " + str(Writer.stringlen(wri_small, moscowTime)))
-    print("Length mempoolFees: " + str(Writer.stringlen(wri_tiny, mempoolFees)))
     refresh(ssd, True)
     ssd.wait_until_ready()
+    #time.sleep(10)
     Label(
         wri_small,
         labelRow1,
-        int((displayLength - Writer.stringlen(wri_small, blockHeight)) / 2),
+        int(
+            (
+                displayLength
+                - Writer.stringlen(wri_small, blockHeight)
+                + Writer.stringlen(wri_iconsSmall, "A")
+             )
+             / 2
+        ),
         blockHeight,
+    )
+    
+    Label(
+        wri_iconsSmall,
+        labelRow1,
+        int(
+            (
+                displayLength
+                - Writer.stringlen(wri_iconsSmall, "A")
+                - Writer.stringlen(wri_small, blockHeight )
+             )
+             / 2
+        ),
+        "A",
     )
     Label(
         wri_large,
@@ -47,35 +67,57 @@ def main():
             (
                 displayLength
                 - Writer.stringlen(wri_large, moscowTime)
-                + Writer.stringlen(wri_satoshi, satoshiSymbol)
+                + Writer.stringlen(wri_iconsLarge, "E")
             )
             / 2
         ),
         moscowTime,
     )
     Label(
-        wri_satoshi,
+        wri_iconsLarge,
         labelRow2,
         int(
             (
                 displayLength
-                - Writer.stringlen(wri_satoshi, satoshiSymbol)
+                - Writer.stringlen(wri_iconsLarge, "E")
                 - Writer.stringlen(wri_large, moscowTime)
             )
             / 2
         ),
-        satoshiSymbol,
+        "E",
     )
     Label(
-        wri_tiny,
+        wri_small,
         labelRow3,
-        int((displayLength - Writer.stringlen(wri_tiny, mempoolFees)) / 2),
+        int(
+            (
+                displayLength
+                - Writer.stringlen(wri_small, mempoolFees)
+                + Writer.stringlen(wri_iconsSmall, "C")
+             )
+             / 2
+        ),
         mempoolFees,
     )
+    Label(
+        wri_iconsSmall,
+        labelRow3,
+        int(
+            (
+                displayLength
+                - Writer.stringlen(wri_iconsSmall, "C")
+                - Writer.stringlen(wri_small, mempoolFees)
+             )
+             / 2
+        ),
+        "C",
+    )
+            
     ssd.wait_until_ready()
     refresh(ssd, False)
     ssd.wait_until_ready()
     ssd.sleep()
-
+    print("sleep")
 
 main()
+
