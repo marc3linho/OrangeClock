3dbef2207a4758d55df077b351c43a32a58c74e5
Jean Do <jean.dominique.lecomte.delisle@protonmail.me>
2024-03-18 22:05:42 -0400

    external data separated into datastore

---
+++ src/orangeClockFunctions/datastore.py
@@ -0,0 +1,106 @@
+import time
+import requests
+
+
+class ExternalData:
+    def __init__(self, url, ttl=300, json=True):
+        self.url = url
+        self.ttl = ttl
+        self.json = json
+        self.updated = None
+        self.data = None
+        self.refresh()
+
+    def __str__(self):
+        return 'ExternalData("{}")'.format(self.url)
+
+    def refresh(self):
+        now = time.time()
+        answer = None
+
+        if self.updated and self.updated + self.ttl > now:
+            return answer
+
+        try:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                if self.json:
+                    data = response.json()
+                else:
+                    data = response.text
+                self.updated = now
+            else:
+                print("status_code {}: requests.get({})".format(status_code, self.url))
+                data = self.data
+                answer = False
+        except Exception as err:
+            print("Exception {}: requests.get({})".format(err, self.url))
+            data = self.data
+            answer = False
+        finally:
+            try: response.close()
+            except Exception: pass
+
+        if data != self.data:
+            self.data = data
+            answer = True
+
+        return answer
+
+#
+# _extdata is a singleton dict holding ExternalData instances -- used internally
+#
+_extdata = None
+
+
+#
+# functions for updating the _extdata singleton
+#
+
+def initialize():
+    global _extdata
+    _extdata = {
+        "prices": ExternalData("https://mempool.space/api/v1/prices", 300),
+        "fees": ExternalData("https://mempool.space/api/v1/fees/recommended", 120),
+        "height": ExternalData("https://mempool.space/api/blocks/tip/height", 180, json=False),
+    }
+
+def set_nostr_pubkey(npub):
+    _extdata['zaps'] = ExternalData("https://api.nostr.band/v0/stats/profile/"+npub, 300)
+
+def refresh(raise_on_failure=False):
+    refreshed = []
+    failures = []
+    for key, datum in _extdata.items():
+        result = datum.refresh()
+        if result == False:
+            failures.append(key)
+        elif result == True:
+            refreshed.append(key)
+        else:
+            pass # no change
+    if failures:
+        msg = "datastore.refresh() had failures: {}".format(",".join(failures))
+        if raise_on_failure:
+            raise Exception(msg)
+        else:
+            print(msg)
+    return refreshed
+
+
+#
+# functions for getting "raw" values from the _extdata singleton
+#
+
+def get_height():
+    return int(_extdata["height"].data)
+
+def get_price(key): # USD, EUR
+    return _extdata["prices"].data[key]
+
+def get_fees_dict():
+    return _extdata["fees"].data
+
+def get_nostr_zap_count():
+    pubkey = [x for x in _extdata["zaps"].data['stats'].keys()][0]
+    return _extdata["zaps"].data["stats"][pubkey]["zaps_received"]["count"]
--- src/orangeClockFunctions/displayBlockMoscowFees.py
+++ src/orangeClockFunctions/displayBlockMoscowFees.py
@@ -2,11 +2,10 @@ from color_setup import ssd
 from gui.core.writer import Writer
 from gui.core.nanogui import refresh
 from gui.widgets.label import Label
+from orangeClockFunctions import datastore
 
 import network
 import time
-import urequests
-import json
 import gui.fonts.orangeClockIcons25 as iconsSmall
 import gui.fonts.orangeClockIcons35 as iconsLarge
 import gui.fonts.libreFranklinBold50 as large
@@ -31,6 +30,7 @@ secretsPASSWORD = ""
 dispVersion1 = "bh"  #bh = block height / hal = halving countdown / zap = Nostr zap counter
 dispVersion2 = "mts" #mts = moscow time satsymbol / mts2 = moscow time satusd icon / mt = without satsymbol / fp1 = fiat price [$] / fp2 = fiat price [€]
 npub = ""
+
 
 def connectWIFI():
     global wifi
@@ -57,44 +57,23 @@ def setSecrets(SSID, PASSWORD):
     secretsPASSWORD = PASSWORD
 
 
-def getPrice(currency): # change USD to EUR for price in euro
-    gc.collect()
-    data = urequests.get("https://mempool.space/api/v1/prices")
-    price = data.json()[currency]
-    data.close()
-    return price
-
-
 def getMoscowTime():
-    moscowTime = str(int(100000000 / float(getPrice("USD"))))
-    return moscowTime
+    return str(int(100000000 / float(datastore.get_price("USD"))))
 
 
 def getPriceDisplay(currency):
-    price_str = f"{getPrice(currency):,}"
+    price_str = f"{datastore.get_price(currency):,}"
     if currency == "EUR":
         price_str = price_str.replace(",", ".")
     return price_str
 
 
 def getLastBlock():
-    gc.collect()
-    data = urequests.get("https://mempool.space/api/blocks/tip/height")
-    block = data.text
-    data.close()
-    return block
-
-
-def getMempoolFees():
-    gc.collect()
-    data = urequests.get("https://mempool.space/api/v1/fees/recommended")
-    jsonData = data.json()
-    data.close()
-    return jsonData
+    return str(datastore.get_height())
 
 
 def getMempoolFeesString():
-    mempoolFees = getMempoolFees()
+    mempoolFees = datastore.get_fees_dict()
     mempoolFeesString = (
         "L:"
         + str(mempoolFees["hourFee"])
@@ -106,16 +85,12 @@ def getMempoolFeesString():
     return mempoolFeesString
 
 
-def getNostrZapCount(nPub):
-    gc.collect()
-    data = urequests.get("https://api.nostr.band/v0/stats/profile/"+nPub)
-    jsonData = str(data.json()["stats"][str(data.json())[12:76]]["zaps_received"]["count"])
-    data.close()
-    return jsonData
+def getNostrZapCount():
+    return str(datastore.get_nostr_zap_count())
 
 
 def getNextHalving():
-    return str(210000 * (math.trunc(int(getLastBlock()) / 210000) + 1) - int(getLastBlock()))
+    return str(210000 - datastore.get_height() % 210000)
 
 
 def displayInit():
@@ -131,10 +106,13 @@ def displayInit():
 
 
 def debugConsoleOutput(id):
-    print("===============debug id= " + id + "===============")
-    print("memory use: ", gc.mem_alloc() / 1024, "KiB")
+    mem_alloc = gc.mem_alloc()
+    print("=============== debug id=" + id + " ===============")
+    print("memory used: ", mem_alloc / 1024, "KiB")
     print("memory free: ", gc.mem_free() / 1024, "KiB")
-    print("===============end debug===============")
+    gc.collect()
+    print("gc.collect() freed additional:", (mem_alloc - gc.mem_alloc()) / 1024, "KiB")
+    print("=============== end debug ===============")
 
 
 def main():
@@ -150,6 +128,11 @@ def main():
     i = 1
     connectWIFI()
     displayInit()
+
+    datastore.initialize()
+    if npub:
+        datastore.set_nostr_pubkey(npub)
+
     while True:
         debugConsoleOutput("2")
         if issue:
@@ -168,9 +151,17 @@ def main():
             refresh(ssd, True)
             time.sleep(5)
         try:
+            new_data = datastore.refresh(raise_on_failure=True)
+            if new_data:
+                print("datastore.refresh() had updates: {}".format(",".join(new_data)))
+        except Exception as err:
+            debugConsoleOutput("datastore.refresh() failure")
+            print(err)
+            issue = True
+        try:
             if dispVersion1 == "zap":
                 symbolRow1 = "I"
-                blockHeight = getNostrZapCount(npub)
+                blockHeight = getNostrZapCount()
             elif dispVersion1 == "hal":
                 symbolRow1 = "H"
                 blockHeight = getNextHalving()
@@ -307,7 +298,6 @@ def main():
         ssd.sleep()
         if not issue:
             time.sleep(600)  # 600 normal
-
         else:
             wifi.disconnect()
             debugConsoleOutput("6")
